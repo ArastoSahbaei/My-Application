e69c1e6084c6db6f3ea0f8693e6bc81e4393843d
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-02-27 14:25:52 +0100

    x

--- src/component/forms/LoginForm.js
+++ src/component/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from "prop-types"
-import { Form, Button } from "semantic-ui-react"
+import { Form, Button, Message } from "semantic-ui-react"
 import Validator from "validator"
 import InlineError from "../messages/InlineError"
 
@@ -22,7 +22,10 @@ export default class LoginForm extends Component {
         const errors = this.validate(this.state.data)
         this.setState({ errors })
         if (Object.keys(errors).length === 0) {
+            this.setState({ loading: true })
             this.props.submit(this.state.data)
+            .catch(err => this.setState({loading: false })
+            )
         }
     }
 
@@ -35,11 +38,11 @@ export default class LoginForm extends Component {
 
   render() {
 
-    const { data, errors } = this.state
+    const { data, errors, loading } = this.state
 
     return (
       <div>
-          <Form onSubmit = {this.onSubmit}>
+          <Form onSubmit = {this.onSubmit} loading= {loading}>
             <Form.Field error = {!!errors.email}>
                         <label htmlFor="email">Email</label>
                         <input 
