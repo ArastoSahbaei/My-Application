20382bdd26360bb403cfc5b6735a23d2fa51546b
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-04 11:06:58 +0100

    only authenticated users can accec the dashboard

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Route } from "react-router-dom"
 import HomePage from "./component/HomePage/HomePage"
 import LoginPage from "./containers/LoginPage/LoginPage"
+import DashBoardPage from "./containers/dashBoardPage/DashBoardPage"
+import UserRoute from "./component/routes/UserRoute"
+
+
+
 
 const App = () => 
               <div className="ui container">
-                <Route path="/" exact component={HomePage} />
-                <Route path="/login" exact component={LoginPage} />
+                <Route path="/"           exact component = {HomePage} />
+                <Route path="/login"      exact component = {LoginPage} />
+                <UserRoute path="/dashboard"  exact component = {DashBoardPage} />
               </div>
 
 export default App;
---
+++ src/containers/dashBoardPage/DashBoardPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DashBoardPage = () => (
+
+    <div>
+        
+    </div>
+)
+
+export default DashBoardPage
