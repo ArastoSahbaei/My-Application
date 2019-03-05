6da512c5c6f03387324027977e4b0a853bd9fdcc
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-05 12:56:59 +0100

    added lawpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Route } from "react-router-dom"
 import HomePage from "./component/HomePage/HomePage"
-import LoginPage from "./containers/LoginPage/LoginPage"
+import LoginPage from "./containers/loginPage/LoginPage"
 import DashBoardPage from "./containers/dashBoardPage/DashBoardPage"
 import UserRoute from "./component/routes/UserRoute"
+import LawPage from "./containers/lawPage/LawPage"
 
 const App = () => 
               <div className="ui container">
-              
+            
                 <Route path="/"                exact component = {HomePage} />
                 <Route path="/login"           exact component = {LoginPage} />
                 <UserRoute path="/dashboard"   exact component = {DashBoardPage} />
+                <UserRoute path="/law"         exact component = {LawPage} />
+
               </div>
 
 export default App;
---
+++ src/containers/lawPage/LawPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavigationBar from "../../component/navigation/NavigationBar"
+
+/* const viProvar = () => {
+    alert("WELCOME " + sessionStorage.getItem("email"))
+    console.log(sessionStorage.getItem("email"))
+} */
+
+const LawPage = () => (
+
+    <div>
+        <NavigationBar />
+        <h1>This is the Law-Page</h1>
+    </div>
+)
+
+export default LawPage
