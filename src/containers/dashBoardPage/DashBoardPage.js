551d272867bd2ebed9839413be582c814eaf92e2
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-13 14:13:51 +0100

    now in card

--- src/containers/dashBoardPage/DashBoardPage.js
+++ src/containers/dashBoardPage/DashBoardPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '@material-ui/core/Card'
 
 /*  const viProvar = () => {
     (sessionStorage.getItem("email"))
@@ -7,55 +8,62 @@ import React from 'react'
 const DashBoardPage = () => (
 
     <div>
+        <Card>
         <h1>Välkommen, {sessionStorage.getItem("email")} !</h1>
-      Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
-      Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
-  
+          Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
+          Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
+          </Card>
+          <br />
+          <Card>
         <h3>2018-11-06 Uppdatering gjord av lagbevakningen</h3>
         Et laboris consectetur pariatur in ea deserunt dolor cillum ea velit sint exercitation sint aute.
-Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
-       
+        Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
+        </Card>
+        <br />
+        <Card>
         <h3>2018-10-31 Prishöjning 2019</h3>
         Aliquip deserunt fugiat incididunt excepteur labore. Eiusmod nulla incididunt labore sit anim amet ea duis exercitation esse. Duis qui fugiat magna voluptate labore incididunt consequat ipsum. Ea cillum sunt anim cupidatat aliqua pariatur aute quis nulla proident minim. Consectetur laborum est reprehenderit ut cillum consectetur ut ea mollit amet. Culpa proident mollit sint aute sunt laborum aliquip fugiat ipsum nostrud elit in nostrud nostrud.
-
+        </Card>
+        <br />
+        <Card>
         <h3>2018-05-04 Ramboll förvärvar RSM&CO</h3>
         Duis do proident adipisicing consequat. Magna nulla est non eu et amet esse minim. Qui cillum eu sint est irure consequat mollit pariatur. Tempor sunt nostrud duis consequat excepteur esse ut proident dolore. Ut laboris irure duis velit minim anim irure ex voluptate.
         Adipisicing sunt nisi ea tempor incididunt et enim ea in ad labore et. Voluptate tempor cillum ipsum excepteur sint deserunt reprehenderit excepteur. Nulla fugiat duis ea cupidatat cupidatat eu quis reprehenderit dolor non aute velit proident amet. Sint anim labore ipsum mollit dolore cupidatat non sunt consequat cupidatat commodo irure aute. Tempor ut eu veniam ad aute.
-
+        </Card>
+<br />
+        <Card>
         <h3>2018-05-04 Demo av lagbevakningen</h3>
         Laborum laboris ad eiusmod enim consectetur minim. Amet nisi reprehenderit tempor duis culpa consectetur. Eu duis laboris consectetur voluptate. Et ut eiusmod ex aliquip commodo do est velit. Aliqua elit mollit deserunt elit consequat nostrud officia dolore sunt. Mollit cillum velit occaecat est minim occaecat aliqua esse fugiat quis nulla esse.
       Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
       Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
-  
+      </Card>
+       
+          <br />
+          <Card>
         <h3>2018-11-06 Uppdatering gjord av lagbevakningen</h3>
         Et laboris consectetur pariatur in ea deserunt dolor cillum ea velit sint exercitation sint aute.
-Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
-       
+        Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
+        </Card>
+        <br />
+        <Card>
         <h3>2018-10-31 Prishöjning 2019</h3>
         Aliquip deserunt fugiat incididunt excepteur labore. Eiusmod nulla incididunt labore sit anim amet ea duis exercitation esse. Duis qui fugiat magna voluptate labore incididunt consequat ipsum. Ea cillum sunt anim cupidatat aliqua pariatur aute quis nulla proident minim. Consectetur laborum est reprehenderit ut cillum consectetur ut ea mollit amet. Culpa proident mollit sint aute sunt laborum aliquip fugiat ipsum nostrud elit in nostrud nostrud.
-
+        </Card>
+        <br />
+        <Card>
         <h3>2018-05-04 Ramboll förvärvar RSM&CO</h3>
         Duis do proident adipisicing consequat. Magna nulla est non eu et amet esse minim. Qui cillum eu sint est irure consequat mollit pariatur. Tempor sunt nostrud duis consequat excepteur esse ut proident dolore. Ut laboris irure duis velit minim anim irure ex voluptate.
         Adipisicing sunt nisi ea tempor incididunt et enim ea in ad labore et. Voluptate tempor cillum ipsum excepteur sint deserunt reprehenderit excepteur. Nulla fugiat duis ea cupidatat cupidatat eu quis reprehenderit dolor non aute velit proident amet. Sint anim labore ipsum mollit dolore cupidatat non sunt consequat cupidatat commodo irure aute. Tempor ut eu veniam ad aute.
-
+        </Card>
+<br />
+        <Card>
         <h3>2018-05-04 Demo av lagbevakningen</h3>
         Laborum laboris ad eiusmod enim consectetur minim. Amet nisi reprehenderit tempor duis culpa consectetur. Eu duis laboris consectetur voluptate. Et ut eiusmod ex aliquip commodo do est velit. Aliqua elit mollit deserunt elit consequat nostrud officia dolore sunt. Mollit cillum velit occaecat est minim occaecat aliqua esse fugiat quis nulla esse.
       Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
       Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
-  
-        <h3>2018-11-06 Uppdatering gjord av lagbevakningen</h3>
-        Et laboris consectetur pariatur in ea deserunt dolor cillum ea velit sint exercitation sint aute.
-Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
-       
-        <h3>2018-10-31 Prishöjning 2019</h3>
-        Aliquip deserunt fugiat incididunt excepteur labore. Eiusmod nulla incididunt labore sit anim amet ea duis exercitation esse. Duis qui fugiat magna voluptate labore incididunt consequat ipsum. Ea cillum sunt anim cupidatat aliqua pariatur aute quis nulla proident minim. Consectetur laborum est reprehenderit ut cillum consectetur ut ea mollit amet. Culpa proident mollit sint aute sunt laborum aliquip fugiat ipsum nostrud elit in nostrud nostrud.
-
-        <h3>2018-05-04 Ramboll förvärvar RSM&CO</h3>
-        Duis do proident adipisicing consequat. Magna nulla est non eu et amet esse minim. Qui cillum eu sint est irure consequat mollit pariatur. Tempor sunt nostrud duis consequat excepteur esse ut proident dolore. Ut laboris irure duis velit minim anim irure ex voluptate.
-        Adipisicing sunt nisi ea tempor incididunt et enim ea in ad labore et. Voluptate tempor cillum ipsum excepteur sint deserunt reprehenderit excepteur. Nulla fugiat duis ea cupidatat cupidatat eu quis reprehenderit dolor non aute velit proident amet. Sint anim labore ipsum mollit dolore cupidatat non sunt consequat cupidatat commodo irure aute. Tempor ut eu veniam ad aute.
-
-        <h3>2018-05-04 Demo av lagbevakningen</h3>
-        Laborum laboris ad eiusmod enim consectetur minim. Amet nisi reprehenderit tempor duis culpa consectetur. Eu duis laboris consectetur voluptate. Et ut eiusmod ex aliquip commodo do est velit. Aliqua elit mollit deserunt elit consequat nostrud officia dolore sunt. Mollit cillum velit occaecat est minim occaecat aliqua esse fugiat quis nulla esse.
+      </Card>
+   
+   
     </div>
 )
 
