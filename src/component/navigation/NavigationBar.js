8483afb7e0b0a31ec3755953e22b1eff8d8784b5
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-14 14:08:23 +0100

    language choice

--- src/component/navigation/NavigationBar.js
+++ src/component/navigation/NavigationBar.js
@@ -19,37 +19,33 @@ const { user, logout } = this.props
     return (
   <Menu secondary pointing>
 
-    <Menu.Item as={Link} to="/dashboard">  <i className="fa fa-home fa-lg"/>           <FormattedMessage id="navigationBar.nav1"/> </Menu.Item>
-    <Menu.Item as={Link} to="/law">        <i className="fa fa-gavel fa-lg"/>          <FormattedMessage id="navigationBar.nav2"/> </Menu.Item>
-    <Menu.Item as={Link} to="/revisions">  <i className="fa fa-book fa-lg"/>           <FormattedMessage id="navigationBar.nav3"/> </Menu.Item>
-    <Menu.Item as={Link} to="/changes">    <i className="fas fa-exchange-alt fa-lg"/>  <FormattedMessage id="navigationBar.nav4"/> </Menu.Item>
-    <Menu.Item as={Link} to="/development">    <i className="fas fa-code fa-lg"/>      <FormattedMessage id="navigationBar.nav5"/> </Menu.Item>
-
-
-    <a role="button" onClick={() => this.props.setLocale("en")}>EN</a> ||||
-    <a role="button" onClick={() => this.props.setLocale("se")}>SE</a>
-
+    <Menu.Item as={Link} to="/dashboard">     <i className="fa fa-home fa-lg"/>           <FormattedMessage id="navigationBar.nav1"/> </Menu.Item>
+    <Menu.Item as={Link} to="/law">           <i className="fa fa-gavel fa-lg"/>          <FormattedMessage id="navigationBar.nav2"/> </Menu.Item>
+    <Menu.Item as={Link} to="/revisions">     <i className="fa fa-book fa-lg"/>           <FormattedMessage id="navigationBar.nav3"/> </Menu.Item>
+    <Menu.Item as={Link} to="/changes">       <i className="fas fa-exchange-alt fa-lg"/>  <FormattedMessage id="navigationBar.nav4"/> </Menu.Item>
+    <Menu.Item as={Link} to="/development">   <i className="fas fa-code fa-lg"/>          <FormattedMessage id="navigationBar.nav5"/> </Menu.Item>
 
     <Menu.Menu position="right">
        <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)} style={{'fontSize':18}} />}>
             <Dropdown.Menu>
                 <Dropdown.Item as={Link} to="/dashboard1"> <i className="fas fa-user"/>      Profile      </Dropdown.Item>
-
-                
-                <div class="ui dropdown item"> Languages <i class="dropdown icon"></i>
-    <div class="menu">
-      <div class="item">Choice 1</div>
-      <div class="item">Choice 2</div>
-      <div class="item">Choice 3</div>
-    </div>
-  </div>
+                                  <Dropdown item text='Display Options'>
+                                      <Dropdown.Menu>
+                                        <Dropdown.Header>Choose Language</Dropdown.Header>
+                                        <Dropdown.Item onClick={() => this.props.setLocale("en")}>English</Dropdown.Item>
+                                        <Dropdown.Item onClick={() => this.props.setLocale("se")}>Swedish</Dropdown.Item>
+                                        <Dropdown.Item>Norsk</Dropdown.Item>
+                                        <Dropdown.Item>Dansk</Dropdown.Item>
+                                      </Dropdown.Menu>
+                                 </Dropdown>
                 <Dropdown.Item as={Link} to="/dashboard2"> <i className="fas fa-tools"/>     Management   </Dropdown.Item>
                 <Dropdown.Item as={Link} to="/dashboard3"> <i className="fas fa-cog"/>       My Settings  </Dropdown.Item> <hr/>
                 <Dropdown.Item onClick={() => logout()}><i className="fas fa-sign-out-alt"/> Sign Out     </Dropdown.Item>
-                
             </Dropdown.Menu>
       </Dropdown>
     </Menu.Menu>
+
+
   </Menu>
 )
     }}
