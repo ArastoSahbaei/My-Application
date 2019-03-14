30d5d017b56ad9f2731beb35845be327e821ffa1
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-14 11:33:29 +0100

    dashboardPage updated

--- src/containers/dashBoardPage/DashBoardPage.js
+++ src/containers/dashBoardPage/DashBoardPage.js
@@ -1,70 +1,104 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Avatar from '@material-ui/core/Avatar'
+import Typography from '@material-ui/core/Typography'
+import red from '@material-ui/core/colors/red'
+import "./DashBoardPage.css"
 
-/*  const viProvar = () => {
-    (sessionStorage.getItem("email"))
-}  */
+const styles = theme => ({
+  card: {
+    maxWidth: 1000,
+    margin: 5,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
+  actions: {
+    display: 'flex',
+  },
+  title: {
+    color: 'black',
+    fontSize: 25
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+})
 
-const DashBoardPage = () => (
+class dashBoardPage extends React.Component {
+  state = { expanded: false }
 
-  <Card>
-        <Card>
-        <h1>Välkommen, {sessionStorage.getItem("email")} !</h1>
-          Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
-          Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
-          </Card>
-          <br />
-          <Card>
-        <h3>2018-11-06 Uppdatering gjord av lagbevakningen</h3>
-        Et laboris consectetur pariatur in ea deserunt dolor cillum ea velit sint exercitation sint aute.
-        Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
-        </Card>
-        <br />
-        <Card>
-        <h3>2018-10-31 Prishöjning 2019</h3>
-        Aliquip deserunt fugiat incididunt excepteur labore. Eiusmod nulla incididunt labore sit anim amet ea duis exercitation esse. Duis qui fugiat magna voluptate labore incididunt consequat ipsum. Ea cillum sunt anim cupidatat aliqua pariatur aute quis nulla proident minim. Consectetur laborum est reprehenderit ut cillum consectetur ut ea mollit amet. Culpa proident mollit sint aute sunt laborum aliquip fugiat ipsum nostrud elit in nostrud nostrud.
-        </Card>
-        <br />
-        <Card>
-        <h3>2018-05-04 Ramboll förvärvar RSM&CO</h3>
-        Duis do proident adipisicing consequat. Magna nulla est non eu et amet esse minim. Qui cillum eu sint est irure consequat mollit pariatur. Tempor sunt nostrud duis consequat excepteur esse ut proident dolore. Ut laboris irure duis velit minim anim irure ex voluptate.
-        Adipisicing sunt nisi ea tempor incididunt et enim ea in ad labore et. Voluptate tempor cillum ipsum excepteur sint deserunt reprehenderit excepteur. Nulla fugiat duis ea cupidatat cupidatat eu quis reprehenderit dolor non aute velit proident amet. Sint anim labore ipsum mollit dolore cupidatat non sunt consequat cupidatat commodo irure aute. Tempor ut eu veniam ad aute.
-        </Card>
-<br />
-        <Card>
-        <h3>2018-05-04 Demo av lagbevakningen</h3>
-        Laborum laboris ad eiusmod enim consectetur minim. Amet nisi reprehenderit tempor duis culpa consectetur. Eu duis laboris consectetur voluptate. Et ut eiusmod ex aliquip commodo do est velit. Aliqua elit mollit deserunt elit consequat nostrud officia dolore sunt. Mollit cillum velit occaecat est minim occaecat aliqua esse fugiat quis nulla esse.
-      Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
-      Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
+  render() {
+    const { classes } = this.props
+
+    return (
+      <div>
+      <Card className={classes.card}>
+          <CardHeader avatar={ <Avatar aria-label="Recipe" className={classes.avatar}> Paul </Avatar> }
+            classes={{ title: classes.title }}
+            title="Uppdatering gjord av lagbevakningen"
+            subheader="Juni 14, 2019" />
+    {/* <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish" /> */}
+            <CardContent>
+                <Typography component="p">
+                      Ullamco exercitation reprehenderit cupidatat deserunt nisi occaecat aute sint. Duis amet sint do esse voluptate est ut. Quis id elit est consectetur est aute dolore ipsum. Consequat esse officia et deserunt irure laboris esse. Veniam occaecat consectetur dolore pariatur nisi. Ullamco in tempor duis elit reprehenderit ex ut ad eiusmod duis ut.
+                      Officia magna ad sint qui adipisicing amet exercitation nostrud ut ipsum fugiat. Incididunt eu adipisicing cupidatat id ex fugiat ex. Excepteur esse nostrud cupidatat commodo non. Laboris laboris nisi minim magna pariatur ullamco aliqua tempor veniam adipisicing minim veniam nisi excepteur. Consectetur irure quis reprehenderit ea incididunt et ea et. Dolor nulla officia ea esse elit aute incididunt aliquip qui sunt. Aute id irure ad nisi excepteur ut aliquip sunt veniam commodo.
+                  </Typography>
+            </CardContent>
       </Card>
-       
-          <br />
-          <Card>
-        <h3>2018-11-06 Uppdatering gjord av lagbevakningen</h3>
-        Et laboris consectetur pariatur in ea deserunt dolor cillum ea velit sint exercitation sint aute.
-        Aliqua officia aute laboris pariatur fugiat minim Lorem eu reprehenderit. Nostrud ad cillum velit pariatur aliqua consequat nisi irure eu aliquip ex. Fugiat qui cillum id laboris est tempor non ex proident eiusmod ipsum. Fugiat enim laborum ad officia cupidatat labore exercitation est qui culpa aute quis culpa.
-        </Card>
-        <br />
-        <Card>
-        <h3>2018-10-31 Prishöjning 2019</h3>
-        Aliquip deserunt fugiat incididunt excepteur labore. Eiusmod nulla incididunt labore sit anim amet ea duis exercitation esse. Duis qui fugiat magna voluptate labore incididunt consequat ipsum. Ea cillum sunt anim cupidatat aliqua pariatur aute quis nulla proident minim. Consectetur laborum est reprehenderit ut cillum consectetur ut ea mollit amet. Culpa proident mollit sint aute sunt laborum aliquip fugiat ipsum nostrud elit in nostrud nostrud.
-        </Card>
-        <br />
-        <Card>
-        <h3>2018-05-04 Ramboll förvärvar RSM&CO</h3>
-        Duis do proident adipisicing consequat. Magna nulla est non eu et amet esse minim. Qui cillum eu sint est irure consequat mollit pariatur. Tempor sunt nostrud duis consequat excepteur esse ut proident dolore. Ut laboris irure duis velit minim anim irure ex voluptate.
-        Adipisicing sunt nisi ea tempor incididunt et enim ea in ad labore et. Voluptate tempor cillum ipsum excepteur sint deserunt reprehenderit excepteur. Nulla fugiat duis ea cupidatat cupidatat eu quis reprehenderit dolor non aute velit proident amet. Sint anim labore ipsum mollit dolore cupidatat non sunt consequat cupidatat commodo irure aute. Tempor ut eu veniam ad aute.
-        </Card>
-<br />
-        <Card>
-        <h3>2018-05-04 Demo av lagbevakningen</h3>
-        Laborum laboris ad eiusmod enim consectetur minim. Amet nisi reprehenderit tempor duis culpa consectetur. Eu duis laboris consectetur voluptate. Et ut eiusmod ex aliquip commodo do est velit. Aliqua elit mollit deserunt elit consequat nostrud officia dolore sunt. Mollit cillum velit occaecat est minim occaecat aliqua esse fugiat quis nulla esse.
-      Reprehenderit do labore nisi duis cupidatat consequat ut commodo incididunt ipsum elit. Officia mollit officia magna aliquip amet commodo in pariatur sit. Nisi eiusmod aliqua voluptate fugiat. Duis ullamco dolor deserunt reprehenderit. Consectetur pariatur laboris ut quis consectetur quis occaecat laborum magna sit incididunt. Do aute ex incididunt reprehenderit proident deserunt ad.
-      Aliquip cillum exercitation velit do Lorem voluptate ipsum ipsum aute incididunt elit mollit. Est anim nisi in do ut et. Consectetur ullamco ad veniam fugiat aute ut laboris eu ad sunt laborum exercitation magna laboris. Laboris ea ad labore quis dolore labore et veniam.
-      </Card>
-   
-   
-      </Card>
-)
 
-export default DashBoardPage
+      <Card className={classes.card}>
+          <CardHeader avatar={ <Avatar aria-label="Recipe" className={classes.avatar}> Paul </Avatar> }
+            classes={{ title: classes.title }}
+            title="Uppdatering gjord av lagbevakningen"
+            subheader="September 14, 2016" />
+    {/* <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish" /> */}
+            <CardContent>
+                <Typography component="p">
+                      Ullamco exercitation reprehenderit cupidatat deserunt nisi occaecat aute sint. Duis amet sint do esse voluptate est ut. Quis id elit est consectetur est aute dolore ipsum. Consequat esse officia et deserunt irure laboris esse. Veniam occaecat consectetur dolore pariatur nisi. Ullamco in tempor duis elit reprehenderit ex ut ad eiusmod duis ut.
+                      Officia magna ad sint qui adipisicing amet exercitation nostrud ut ipsum fugiat. Incididunt eu adipisicing cupidatat id ex fugiat ex. Excepteur esse nostrud cupidatat commodo non. Laboris laboris nisi minim magna pariatur ullamco aliqua tempor veniam adipisicing minim veniam nisi excepteur. Consectetur irure quis reprehenderit ea incididunt et ea et. Dolor nulla officia ea esse elit aute incididunt aliquip qui sunt. Aute id irure ad nisi excepteur ut aliquip sunt veniam commodo.
+                  </Typography>
+            </CardContent>
+      </Card>
+      <Card className={classes.card}>
+          <CardHeader avatar={ <Avatar aria-label="Recipe" className={classes.avatar}> Paul </Avatar> }
+            classes={{ title: classes.title }}
+            title="Uppdatering gjord av lagbevakningen"
+            subheader="September 14, 2016" />
+    {/* <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish" /> */}
+            <CardContent>
+                <Typography component="p">
+                      Ullamco exercitation reprehenderit cupidatat deserunt nisi occaecat aute sint. Duis amet sint do esse voluptate est ut. Quis id elit est consectetur est aute dolore ipsum. Consequat esse officia et deserunt irure laboris esse. Veniam occaecat consectetur dolore pariatur nisi. Ullamco in tempor duis elit reprehenderit ex ut ad eiusmod duis ut.
+                      Officia magna ad sint qui adipisicing amet exercitation nostrud ut ipsum fugiat. Incididunt eu adipisicing cupidatat id ex fugiat ex. Excepteur esse nostrud cupidatat commodo non. Laboris laboris nisi minim magna pariatur ullamco aliqua tempor veniam adipisicing minim veniam nisi excepteur. Consectetur irure quis reprehenderit ea incididunt et ea et. Dolor nulla officia ea esse elit aute incididunt aliquip qui sunt. Aute id irure ad nisi excepteur ut aliquip sunt veniam commodo.
+                  </Typography>
+            </CardContent>
+      </Card>
+      <Card className={classes.card}>
+          <CardHeader avatar={ <Avatar aria-label="Recipe" className={classes.avatar}> Paul </Avatar> }
+            classes={{ title: classes.title }}
+            title="Uppdatering gjord av lagbevakningen"
+            subheader="September 14, 2016" />
+    {/* <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish" /> */}
+            <CardContent>
+                <Typography component="p">
+                      Ullamco exercitation reprehenderit cupidatat deserunt nisi occaecat aute sint. Duis amet sint do esse voluptate est ut. Quis id elit est consectetur est aute dolore ipsum. Consequat esse officia et deserunt irure laboris esse. Veniam occaecat consectetur dolore pariatur nisi. Ullamco in tempor duis elit reprehenderit ex ut ad eiusmod duis ut.
+                      Officia magna ad sint qui adipisicing amet exercitation nostrud ut ipsum fugiat. Incididunt eu adipisicing cupidatat id ex fugiat ex. Excepteur esse nostrud cupidatat commodo non. Laboris laboris nisi minim magna pariatur ullamco aliqua tempor veniam adipisicing minim veniam nisi excepteur. Consectetur irure quis reprehenderit ea incididunt et ea et. Dolor nulla officia ea esse elit aute incididunt aliquip qui sunt. Aute id irure ad nisi excepteur ut aliquip sunt veniam commodo.
+                  </Typography>
+            </CardContent>
+      </Card>
+      </div>
+    )
+  }
+}
+
+
+dashBoardPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(dashBoardPage)
