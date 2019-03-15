32490f3fd2ae04025ff6db9e7feddf44aa716001
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-15 14:05:27 +0100

    osv

--- src/containers/developmentPage/DevelopmentPage.js
+++ src/containers/developmentPage/DevelopmentPage.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react'
-import Axios from 'axios'
+import axios from 'axios'
 export default class Development extends Component {
 
     state = {
-        data: [],
         loading: true,
-        companyName: ""
+        companyName: "",
+        id: null
     }
 
     componentDidMount = () => {
-        Axios.get('http://localhost:8080/lagbevakning/company/byid?id=300')
+        axios.get('http://localhost:8080/lagbevakning/company/byid?id=300')
         .then(response => {
-            this.setState({companyName: response.data.companyName, loading: false})
-            console.log(response.data.companyName)
+            this.setState({companyName: response.data.companyName, loading: false, id: response.data.id})
+            console.log(response.data)
         })
     }
 
   render() {
-
     return (
       <div>
         {this.state.loading || !this.state.companyName 
                             ? <div>Failed to load</div> 
-                            : <div>{this.state.companyName}</div>}
+                            : <div> <h3> Company Name: {this.state.companyName} </h3> </div>}
+                    <hr/>
+                           <h3>Company Id: {this.state.id}</h3>
+
       </div>
 
             
