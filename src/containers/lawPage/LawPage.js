2a996e4f9df93dbe0b3c4fa8dd8156e204293e2c
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-18 13:11:25 +0100

    table implemented

--- src/containers/lawPage/LawPage.js
+++ src/containers/lawPage/LawPage.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
+import { Table } from 'semantic-ui-react'
 import axios from 'axios'
 
 export default class Development extends Component {
 
     state = {
         loading: true,
-        companyName: "testar"
+        companyName: "Loading"
     }
 
 componentDidMount = () => {
 
     axios.get('http://localhost:8080/lagbevakning/subscription/company?id=' + sessionStorage.getItem("id")).then(response => {
         this.setState({
-            companyName: response.data[3].companyItem.companyName,
+            companyName: response.data[0].lawItem.name,
             loading: false
         })
         console.log(response.data)
@@ -25,7 +26,34 @@ render() {
         <div>
              {this.state.loading || !this.state.companyName 
               ? <div>Loading...</div> 
-              : <div> <h3> Company Name: {this.state.companyName} </h3> </div>}
+              : <div> <h3>  {this.state.companyName} </h3> </div>}
+
+<Table singleLine>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Law</Table.HeaderCell>
+        <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>{this.state.companyName}</Table.Cell>
+        <Table.Cell>No</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+      <Table.Cell>{this.state.companyName}</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+      <Table.Cell>{this.state.companyName}</Table.Cell>
+        <Table.Cell>Yes</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+              
         </div>
      )
     }
