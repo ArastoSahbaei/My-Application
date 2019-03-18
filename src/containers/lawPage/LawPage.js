aaa7be07bb1f3342e976e5a0b80867c19fab71be
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-18 08:54:14 +0100

    x

--- src/containers/lawPage/LawPage.js
+++ src/containers/lawPage/LawPage.js
@@ -9,14 +9,16 @@ export default class Development extends Component {
     }
 
 componentDidMount = () => {
-    axios.get('http://localhost:8080/lagbevakning/company/email?email=billy.gustafsson@bilfinger.com')
-    .then(response => {
+
+    const getLoggedInUser = sessionStorage.getItem("email")
+
+    axios.get('http://localhost:8080/lagbevakning/company/email?email=' + getLoggedInUser).then(response => {
         this.setState({companyName: response.data.companyName, id: response.data.id})
-        axios.get('http://localhost:8080/lagbevakning/subscription/company?id=686')
-        .then(response => {
-            console.log(response.data)
-        })
         console.log(response.data)
+        
+        axios.get('http://localhost:8080/lagbevakning/subscription/company?id=686').then(response => {
+        console.log(response.data)
+        })
     })
 }
 
