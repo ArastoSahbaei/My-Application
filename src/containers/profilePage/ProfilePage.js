54963369f512c5c8720cfb6e45d50c0c8bc44b7c
Jonathan Vernersson <Jonathan.Vernersson@alten.se>
2019-03-19 11:13:55 +0100

    Can change password from ProfilePage

--- src/containers/profilePage/ProfilePage.js
+++ src/containers/profilePage/ProfilePage.js
@@ -10,16 +10,20 @@ class ProfilePage extends Component {
   constructor(){
     super();
     this.state = {
-    data: {
-    },
-    oldPassword: "",
-    repeatedNewPassword: "",
-    newPassword: "",
-    currentUser: "",
-    passwordIsSame: false
-  }
+      data: {
+      },
+      oldPassword: "",
+      repeatedNewPassword: "",
+      newPassword: "",
+      currentUser: "",
+      passwordIsSame: false,
+      newEmail: "",
+      newFirstName:"",
+      newLastName:""
+    }
     this.handleChange = this.handleChange.bind(this);
-    };
+
+  };
 
   componentDidMount = () => {
     console.log(this.state.data.oldPassword);
@@ -35,23 +39,51 @@ class ProfilePage extends Component {
     })
 
   handleChange(event) {
-   /*  this.setState({value: event.target.value}); */
-    this.setState({oldPassword: event.target.value})
+    /*  this.setState({value: event.target.value}); */
+    this.setState({[event.target.name]: event.target.value})
     console.log(this.state.oldPassword)
   }
 
+  updateEmail = () => {
+    const newUserInfo ={
+      id : this.state.currentUser.id,
+      email: this.state.newEmail
+    }
+    Axios.put('http://localhost:8080/lagbevakning/user/updateemail', newUserInfo).then(response=>{
+      console.log(response);
+      if(response.status===200){
+        sessionStorage.setItem('email',this.state.newEmail)
+        Axios.get('http://localhost:8080/lagbevakning/user/email?email='+sessionStorage.getItem("email"))
+          .then( response => {
+            this.setState({currentUser: response.data});
+          })
+        console.log("Everythin done");
+      }
+    })
+  }
+
+
   updatePassword = () => {
     if (this.state.data.newPassword !== this.state.data.repeatedNewPassword){
-      this.state.passwordIsSame = true;
+      this.setState({passwordIsSame: false})
       console.log("are not same");
     }
     else{
-      console.log(this.state.data.newPassword + " " + this.state.data.oldPassword);
-      this.state.passWordIsSame = false;
+      console.log(this.state.newPassword + " " + this.state.oldPassword);
+      this.setState({passwordIsSame: true})
+      const newUserInfo ={
+        oldPassword: this.state.oldPassword,
+        newPassword: this.state.newPassword,
+        id: this.state.currentUser.id
+      };
+      Axios.put('http://localhost:8080/lagbevakning/user/updatepassword', newUserInfo).then(response =>{
+        console.log(response);
+        Axios.get('http://localhost:8080/lagbevakning/user/email?email='+sessionStorage.getItem("email"))
+          .then( response => {
+            this.setState({currentUser: response.data});
+          })
+      })
 
-      //Axios.put('http//localhost:8080/lagbevakning/user/updatepassword', {oldPassword, newPassword}).then(response =>{
-       // console.log(console);
-     // })
     }
   }
 
@@ -62,29 +94,59 @@ class ProfilePage extends Component {
 
         <p>Name: {this.state.currentUser.firstName} {this.state.currentUser.lastName}</p>
         <p>Email: {this.state.currentUser.email}</p>
+        {this.editEmail()}
 
-        <Popup class="passwordPopup" trigger={<a className="button"> Change password </a>} modal>
+        <Popup class="passwordPopup" trigger={<a className="changePasswordButton"> Change password </a>} modal>
           {close => (
-           <div className="changePasswordText">
-             <Form className="test" onSubmit = {this.onSubmit}>
+            <div className="changePasswordText">
+              <Form className="test" onSubmit = {this.onSubmit}>
+
              <span><label>Old password <br/></label>
-
-             <input type="password" value={this.state.oldPassword} onChange={this.handleChange}/></span>
-             
-
-             <span><label>New password <br/></label>
              <input
                type="password"
-               value={this.state.data.newPassword}
+               name="oldPassword"
+               onChange={this.handleChange}/></span>
+
+                <span><label>New password <br/></label>
+             <input
+               type="password"
                name="newPassword"
-               onChange={this.handleChange}
-             /></span>
-             <span><label>Repeat new password <br/></label>
-             <input type="password" name="repeatedNewPassword" value={this.state.data.repeatedNewPassword} onChange={this.handleChange}/></span>
-             <button onClick={this.updatePassword.bind()}>Save</button>
-             <button>Cancel</button>
-             </Form>
-           </div>
+               onChange={this.handleChange}/></span>
+
+                <span><label>Repeat new password <br/></label>
+             <input
+               type="password"
+               name="repeatedNewPassword"
+               onChange={this.handleChange}/></span>
+
+                <button
+                  onClick={this.updatePassword.bind()}>Save</button>
+                <button onClick={close.bind()}>Cancel</button>
+              </Form>
+            </div>
+          )}
+        </Popup>
+      </div>
+    )
+  }
+
+  editEmail(){
+    return(
+      <div>
+        <Popup class="emailPopup" trigger={<a className="changeEmailButton"> edit</a>} modal>
+          {close => (
+            <div className="changeEmailText">
+              <Form className="test" onSubmit = {this.onSubmit}>
+                <span><label>New email <br/></label>
+             <input
+               type="email"
+               name="newEmail"
+               onChange={this.handleChange}/></span>
+                <button
+                  onClick={this.updateEmail.bind()}>Save</button>
+                <button onClick={close.bind()}>Cancel</button>
+              </Form>
+            </div>
           )}
         </Popup>
       </div>
