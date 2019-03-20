ecceb75458f7eefe65af526b3c3ae5b8822794c2
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-03-20 10:45:16 +0100

    removed homePageComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react"
-import { Route } from "react-router-dom"
-import HomePage from "./component/HomePage/HomePage"
-import LoginPage from "./containers/LoginPage/LoginPage"
+import LoginPage from "./containers/loginPage/LoginPage"
 import DashBoardPage from "./containers/dashBoardPage/DashBoardPage"
 import UserRoute from "./component/routes/UserRoute"
 import GuestRoute from "./component/routes/GuestRoute"
@@ -24,8 +22,7 @@ class App extends React.Component {
       <IntlProvider locale={lang} messages={messages[lang]}>
               <div className="ui container">
                {isAuthenticated && <NavigationBar />}
-                <Route path="/"                                       exact component = {HomePage} />
-                <GuestRoute location={location} path="/login"         exact component = {LoginPage} />
+                <GuestRoute location={location} path="/"              exact component = {LoginPage} />
                 <UserRoute  location={location} path="/dashboard"     exact component = {DashBoardPage} />
                 <UserRoute  location={location} path="/law"           exact component = {LawPage} />
                 <UserRoute  location={location} path="/development"   exact component = {DevelopmentPage} />
