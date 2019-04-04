f62e7af8ac6e76ec7044bb8e6fcb2a97164767b1
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-04-04 13:40:29 +0200

    m

--- src/component/revisions/editrevision/EditRevision.js
+++ src/component/revisions/editrevision/EditRevision.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { Table } from 'semantic-ui-react'
-import { Menu } from "semantic-ui-react"
 import axios from 'axios/index'
-import { Link } from "react-router-dom"
 import "./EditRevision.css"
 
 export default class EditRevision extends Component {
 
   state = {
-      data: []
+      data: [],
+      customColumns: []
   }
 
   componentDidMount = () => {
@@ -18,33 +17,35 @@ export default class EditRevision extends Component {
         loading: false
       })
     })
+    axios.get('http://localhost:8080/lagbevakning/company?id=' + sessionStorage.getItem("id")).then(response2 => {
+      this.setState({
+        customColumns: response2.data
+      })
+    })
   }
+
     
   render() {
-    console.log(this.props.match.params.id)
+    console.log(this.state.data) 
+    console.log(this.state.customColumns)
+
     return (
       <div>
+       <h2>Revision Name: {this.state.data.name} & Revision id: {this.state.data.id}</h2>
       <Table celled>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>Index</Table.HeaderCell>
-            <Table.HeaderCell> x </Table.HeaderCell>
-            <Table.HeaderCell> x </Table.HeaderCell>
-            <Table.HeaderCell> x </Table.HeaderCell>
-            <Table.HeaderCell> x </Table.HeaderCell>
-            <Table.HeaderCell>Antal lagar</Table.HeaderCell>
-            <Table.HeaderCell>Options</Table.HeaderCell>
+            <Table.HeaderCell> LawName </Table.HeaderCell>
+            <Table.HeaderCell> LawGroupName </Table.HeaderCell>
+            <Table.HeaderCell> Importance </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
           <Table.Row>
-  {/*           <Table.Cell>{index}</Table.Cell>
-            <Table.Cell>{data.name}</Table.Cell>
-            <Table.Cell>{data.comment}</Table.Cell>
-            <Table.Cell>{new Date(data.createdAt).toISOString().substring(0, 10)}</Table.Cell>
-            <Table.Cell>{data.createdBy.firstName + " " + data.createdBy.lastName}</Table.Cell>
-            <Table.Cell>{data.subscriptionCount}</Table.Cell> */}
+            <Table.Cell>{this.state.data.name}</Table.Cell>
+            <Table.Cell>hehe</Table.Cell>
+            <Table.Cell>hehe</Table.Cell>
           </Table.Row>
         </Table.Body>
       </Table>
