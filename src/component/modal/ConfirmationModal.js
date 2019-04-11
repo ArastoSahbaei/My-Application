52b8bbc1af5326218728ca8f1db0083789738c2f
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-04-11 09:40:38 +0200

    added options

--- src/component/modal/ConfirmationModal.js
+++ src/component/modal/ConfirmationModal.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import { Form, TextArea } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 
+const options = [
+ { key: 1, text: 'OK', value: 1 },
+ { key: 2, text: 'Avvikelse', value: 2 },
+ { key: 3, text: 'Ej Relevant', value: 3 },
+ { key: 4, text: 'Observation', value: 4 },
+ { key: 5, text: 'Saknas', value: 5 },
+]
 export default class ConfirmationModal extends React.Component {
-
     TextAreaExampleTextArea = () => (
     <Form>
       <TextArea placeholder='Tell us more' />
     </Form>
   )
+
+   DropdownExampleClearable = () => <Dropdown clearable options={options} selection />
 
     ModalModalExample = () => (
         <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
@@ -22,12 +31,16 @@ export default class ConfirmationModal extends React.Component {
                 address.
               </p>
               {this.TextAreaExampleTextArea()}
+              {this.DropdownExampleClearable()}
               <p>Is it okay to use this photo?</p>
             </Modal.Description>
         </Modal>
       )
 
+
+      
       render() {
+
         return (
           <div>
               {this.ModalModalExample()}
