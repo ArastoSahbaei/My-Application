da57b0d1c9b11b4f6909f7420652dc3784e6fa17
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-04-12 13:03:05 +0200

    added todo

--- src/component/modal/ConfirmationModal.js
+++ src/component/modal/ConfirmationModal.js
@@ -51,29 +51,47 @@ export default class ConfirmationModal extends React.Component {
       </Form>
   )
   
-    openModal = () => (
-    <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
-          <Modal.Header>Revidera Författning</Modal.Header> 
-         {/*  TODO: Hardecoded Law. it should display the current law of the revision */}
-          <Modal.Header>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</Modal.Header>
-          <Modal.Description>
-            <Header> Fyll i kommentar och ange status </Header>
-                {this.textArea()}
-                {this.dropDownList()}
-                {this.submitorCancelButton()}
-          </Modal.Description>
-        </Modal>
-  )
+  saveOnClick = () => {
+    axios.post('/url', {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  cancelOnClick = () => {
+    //This should close the modal.
+    alert("thats how i know my life is out of luck, fool.")
+  }
+  
     /*   TODO: Close on 'Cancel', Post on 'Save' */
     submitorCancelButton = () => (
-        <Button.Group>
-          <Button>Cancel</Button>
+      <Button.Group>
+          <Button onClick={this.cancelOnClick} negative> Cancel </Button>
           <Button.Or />
-          <Button positive>Save</Button>
+          <Button onClick={this.saveOnClick}   positive>  Save  </Button>
         </Button.Group>
   )
-      
-      render() {
+  
+  openModal = () => (
+  <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
+        <Modal.Header>Revidera Författning</Modal.Header> 
+       {/*  TODO: Hardecoded Law. it should display the current law of the revision */}
+        <Modal.Header>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</Modal.Header>
+        <Modal.Description>
+          <Header> Fyll i kommentar och ange status </Header>
+              {this.textArea()}
+              {this.dropDownList()}
+              {this.submitorCancelButton()}
+        </Modal.Description>
+      </Modal>
+  )
+  
+  render() {
         return (
           <div className="x">
               {this.openModal()}
