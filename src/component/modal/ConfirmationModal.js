f1bb3a949e44ce71e0348c0d488be8c5165d84ba
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-04-12 13:03:54 +0200

    ocd

--- src/component/modal/ConfirmationModal.js
+++ src/component/modal/ConfirmationModal.js
@@ -51,7 +51,7 @@ export default class ConfirmationModal extends React.Component {
       </Form>
   )
   
-  saveOnClick = () => {
+    saveOnClick = () => {
     axios.post('/url', {
       firstName: 'Fred',
       lastName: 'Flintstone'
@@ -63,7 +63,7 @@ export default class ConfirmationModal extends React.Component {
       console.log(error);
     });
   }
-  cancelOnClick = () => {
+    cancelOnClick = () => {
     //This should close the modal.
     alert("thats how i know my life is out of luck, fool.")
   }
@@ -77,25 +77,25 @@ export default class ConfirmationModal extends React.Component {
         </Button.Group>
   )
   
-  openModal = () => (
-  <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
-        <Modal.Header>Revidera Författning</Modal.Header> 
-       {/*  TODO: Hardecoded Law. it should display the current law of the revision */}
-        <Modal.Header>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</Modal.Header>
-        <Modal.Description>
-          <Header> Fyll i kommentar och ange status </Header>
-              {this.textArea()}
-              {this.dropDownList()}
-              {this.submitorCancelButton()}
-        </Modal.Description>
-      </Modal>
-  )
+    openModal = () => (
+    <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
+          <Modal.Header>Revidera Författning</Modal.Header> 
+        {/*  TODO: Hardecoded Law. it should display the current law of the revision */}
+          <Modal.Header>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</Modal.Header>
+          <Modal.Description>
+            <Header> Fyll i kommentar och ange status </Header>
+                {this.textArea()}
+                {this.dropDownList()}
+                {this.submitorCancelButton()}
+          </Modal.Description>
+        </Modal>
+    )
   
-  render() {
-        return (
-          <div className="x">
-              {this.openModal()}
-          </div>
-        )
+    render() {
+          return (
+            <div className="x">
+                {this.openModal()}
+            </div>
+          )
+        }
       }
-    }
