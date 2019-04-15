ba959b6d03e3b83752af9f9d8fdf691c21ddc789
Arasto Sahbaei <Arasto.Sahbaei@alten.se>
2019-04-15 09:51:48 +0200

    now closes on click

--- src/component/modal/ConfirmationModal.js
+++ src/component/modal/ConfirmationModal.js
@@ -1,101 +1,93 @@
-import React from 'react'
-import { Button, Header, Modal } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Modal } from 'semantic-ui-react'
 import { Form, TextArea, Dropdown } from 'semantic-ui-react'
 import axios from "axios"
 import "./ConfirmationModal.css"
 
 
-
 const options = [
- { value: 'OK', text: 'OK'},
- { value: 'Avvikelse', text: 'Avvikelse'},
- { value: 'Ej Relevant', text: 'Ej Relevant'},
- { value: 'Observation', text: 'Observation'},
- { value: 'Saknas', text: 'Saknas' },
-]
-export default class ConfirmationModal extends React.Component {
-
+  { value: 'OK', text: 'OK'},
+  { value: 'Avvikelse', text: 'Avvikelse'},
+  { value: 'Ej Relevant', text: 'Ej Relevant'},
+  { value: 'Observation', text: 'Observation'},
+  { value: 'Saknas', text: 'Saknas' },
+ ]
+ export default class ConfirmationModal extends Component {
+  
   state = {
-  data: [],
-  textValue: '',
-  listValue: 'Status',
-  options,
-  value: ''
-  }
+    data: [],
+    textValue: '',
+    listValue: 'Status',
+    options,
+    value: '',
+    open: false
+    }
+    
 
-/*   TODO: take the 'Data' state and replace it with the hardcoded 'options' */
   componentDidMount = () => {
-    axios.get('http://localhost:8080/lagbevakning/revision/revision_items_statuses')
-      .then( response => {
-        this.setState({data: response.data})
-        console.log(response.data)
-      })
+    axios.get('http://localhost:8080/lagbevakning/revision/revision_items_statuses').then( response => {
+      this.setState({data: response.data})
+            console.log(response.data)
+          })
+        }
+
+  dropDownList = () => <Dropdown placeholder={this.state.listValue} onChange={this.handleListChange} clearable options={options} selection />
+
+  handleListChange = (event, {value}) => {
+    this.setState({ value })
+    console.log(value)
   }
 
-    dropDownList = () => <Dropdown placeholder={this.state.listValue} onChange={this.handleListChange} clearable options={options} selection />
+  textArea = () => (
+    <Form>
+      <TextArea placeholder='Skriv en kommentar...' onChange={this.handleTextChange.bind(this)} />
+      {console.log(this.state.textValue)}
+    </Form>
+  )
 
-
-    handleListChange = (event, {value}) => { 
-     this.setState({ value })
-      console.log(value)
-  }
-
-    handleTextChange(event) {
+  handleTextChange(event)  {
       this.setState({textValue: event.target.value})
   }
 
-    textArea = () => (
-      <Form>
-        <TextArea placeholder='Skriv en kommentar...' onChange={this.handleTextChange.bind(this)} />
-        {console.log(this.state.textValue)}
-      </Form>
-  )
-  
-    saveOnClick = () => {
+  saveOnClick = () => {
     axios.post('/url', {
       firstName: 'Fred',
       lastName: 'Flintstone'
     })
     .then(function (response) {
-      console.log(response);
+      console.log(response)
     })
     .catch(function (error) {
-      console.log(error);
-    });
+      console.log(error)
+    })
   }
-    cancelOnClick = () => {
-    //This should close the modal.
-    alert("thats how i know my life is out of luck, fool.")
-  }
-  
-    /*   TODO: Close on 'Cancel', Post on 'Save' */
-    submitorCancelButton = () => (
-      <Button.Group>
-          <Button onClick={this.cancelOnClick} negative> Cancel </Button>
-          <Button.Or />
-          <Button onClick={this.saveOnClick}   positive>  Save  </Button>
-        </Button.Group>
+
+  close = () => this.setState({ open: false })
+  submitorCancelButton = () => (
+    <Button.Group>
+        <Button onClick={this.close}       negative>  Cancel </Button>
+        <Button.Or />
+        <Button onClick={this.saveOnClick} positive>  Save   </Button>
+      </Button.Group>
   )
-  
-    openModal = () => (
-    <Modal trigger={<Button> <i className="far fa-edit"/> </Button>}>
+
+  closeConfigShow = (closeOnEscape, closeOnDimmerClick) => () => {
+      this.setState({ closeOnEscape, closeOnDimmerClick, open: true })
+    }
+
+  render() {
+    const { open, closeOnEscape, closeOnDimmerClick } = this.state
+    return (
+      <div>
+        <Button onClick={this.closeConfigShow(true, false)}>No Close on Dimmer Click</Button>
+        <Modal open={open} onClose={this.close} closeOnEscape={closeOnEscape} closeOnDimmerClick={closeOnDimmerClick}>
           <Modal.Header>Revidera Författning</Modal.Header> 
-        {/*  TODO: Hardecoded Law. it should display the current law of the revision */}
-          <Modal.Header>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</Modal.Header>
-          <Modal.Description>
-            <Header> Fyll i kommentar och ange status </Header>
+          <Modal.Content> <p>SFS 1998:1707 Lag om åtgärder mot buller och avgaser från mobila maskiner</p> </Modal.Content>
                 {this.textArea()}
                 {this.dropDownList()}
                 {this.submitorCancelButton()}
-          </Modal.Description>
         </Modal>
+      </div>
     )
-  
-    render() {
-          return (
-            <div className="x">
-                {this.openModal()}
-            </div>
-          )
-        }
-      }
+  }
+}
